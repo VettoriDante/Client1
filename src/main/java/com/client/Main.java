5fc535771091da913e7171b124107063c59e9792
lai2-24 <lai2-24@itismeucci.com>
2024-10-15 09:18:02 +0200

    done

--- src/main/java/com/client/Main.java
+++ src/main/java/com/client/Main.java
@@ -8,11 +8,12 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.Scanner;
 
-public class Main  {
+public class Main {
     public static void main(String[] args) throws UnknownHostException, IOException{
         System.out.println("client partito");
         Socket s = new Socket("localhost", 3000);
         System.out.println("il client si è collegato");
+        boolean error = false;
 
         BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
         DataOutputStream out = new DataOutputStream(s.getOutputStream());
@@ -24,12 +25,34 @@ public class Main  {
             inputString = scanner.nextLine();
             if(inputString.equals("exit")) inputString = "!";
             out.writeBytes(inputString + "\n");
-
+            do{
+                //chiedo l'operazione da eseguire
+                System.out.println("Decidi l'operazione da eseguire: \n M = maiuscolo \n m = minuscolo \n r = ribalta stringa \n c = conta caratteri \n ex = chiudi programma" );
+                inputString = scanner.nextLine();
+                switch (inputString) {
+                    case "M":
+                    case "m":
+                    case "r":
+                    case "c":
+                    case "ex":
+                        out.writeBytes(inputString + "\n");
+                        error = false;
+                    break;
+                    default:
+                        System.out.println("Valore errato");
+                        error = true;
+                    break;
+                }
+            }while(error);
+            
+            
             String ans = in.readLine();
-            if(inputString.equals("!"))break;
-            System.out.println(ans);
+            if(inputString.equals("ex"))break;
+            if(ans.equals("!!!"))
+                System.out.println("Qualcosa è andato storto");
+            else            
+                System.out.println(ans);
         }while(true);
         scanner.close();
-        s.close();
     }
 }
